ab0d51e8a4b56ab9a31d5d7afda909f9a0de3735
Yasuhiro Yamanaka <yyasuy@gmail.com>
2021-02-11 21:39:00 +0900

    Worked well for 2021 blogs

--- xml.php
+++ xml.php
@@ -3,19 +3,28 @@
 $contents = file_get_contents( 'iamwhatieat.wordpress.2021.xml' );
 $contents = mb_convert_encoding($contents, "UTF-8", "auto");
 $xml = simplexml_load_string( $contents, 'SimpleXMLElement', LIBXML_NOCDATA );
-var_dump( $xml->channel->item[38]->pubDate );
-$text = $xml->channel->item[ 38 ]->children( 'content', true )->encoded;
-var_dump( $text );
 
-$date = $xml->channel->item[38]->pubDate;
-$date = "Sat, 06 Feb 2021 23:07:40 +0000";
-$time = strtotime( $date );
-var_dump( $time );
-var_dump( date( 'Y/m/d', $time ) );
-
-
-$dt = new DateTime( $date );
-var_dump( $dt->format( 'Y/m/d' ) );
-var_dump( $dt->setTimeZone(new DateTimeZone('Asia/Tokyo'))->format( 'Y/m/d D' ) );
+print( "<html>\n" );
+print( "<body>\n" );
+$num_items = count( $xml->channel->item );
+for( $i = $num_items - 1; $i >=0 ; $i-- ){
+	$published = $xml->channel->item[ $i ]->pubDate;
+	$date_time = new DateTime( $published );
+	$date_str = $date_time->setTimeZone(new DateTimeZone('Asia/Tokyo'))->format( 'Y/m/d' );
+	print( "$date_str<br>" );
+	$text = $xml->channel->item[ $i ]->children( 'content', true )->encoded;
+	$text = trim( $text );
+	$lines = explode( "\n", $text );
+	foreach( $lines as $line ){
+		if( preg_match( '|<.+?>|', $line, $matches ) == 1 ){
+			print( "$line" );
+		}else{
+			print( "$line<br>" );
+		}
+	}
+	print( "<br>" );
+}
+print( "</html>\n" );
+print( "</body>\n" );
 ?>
 
